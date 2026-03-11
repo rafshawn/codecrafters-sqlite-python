796b06ecf35581d9fb4d7b6991f1affc47f7e848
Shawn <rafshawn@yandex.com>
2026-03-11 16:52:44 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,12 +9,15 @@ command = sys.argv[2]
 
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
         print("Logs from your program will appear here!", file=sys.stderr)
 
-        # TODO: Uncomment the code below to pass the first stage
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
+
+        database_file.seek(103)  # Skip the db header (100b) + b-tree flag (1b) + first freeblock (2b)
+        number_of_tables = int.from_bytes(database_file.read(2), byteorder="big")
+
         print(f"database page size: {page_size}")
+        print(f"number of tables: {number_of_tables}" )
 else:
     print(f"Invalid command: {command}")
